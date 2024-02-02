c981f5ec971a7dc6affef581c0b9ffda874743dd
wdonsong <wdonsong@gmail.com>
2024-02-02 18:08:10 +0800

    Implement magic link way login with use supabase SDK in hook

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import EmailPasswordSignInForm from "./components/EmailPasswordSignInForm";
+import MagicLinkSignInForm from "./components/MagicLinkSignInForm";
 
 export const metadata = {
   title: "Sign In",
@@ -8,8 +9,8 @@ export const metadata = {
 function SignInPage() {
   return (
     <div className="flex flex-col space-y-4 w-full">
-      <EmailPasswordSignInForm />
-
+      {/* <EmailPasswordSignInForm /> */}
+      <MagicLinkSignInForm />
       <div className="text-sm">
         <span>Don&apos;t have an account yet?</span>{" "}
         <Link className="underline" href="/auth/sign-up">
