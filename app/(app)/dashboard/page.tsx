a6f98cd60ec00c36ab1a988ad429c91556e7e2eb
wdonsong <wdonsong@gmail.com>
2024-02-03 15:14:51 +0800

    Implement show posts in dashboard

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -3,7 +3,14 @@ import { LucideLayoutDashboard } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
+import { redirect } from "next/navigation";
+import { fetchPosts } from "@/lib/queries/posts";
+import getSupabaseServerComponentClient from "@/lib/supabase/server-component-client";
+import { use } from "react";
+
 function DashboardPage() {
+  const posts = use(fetchDashboardPageData());
+
   return (
     <div className="container">
       <div className="flex flex-col flex-1 space-y-8">
@@ -18,9 +25,37 @@ function DashboardPage() {
             <Link href="/new">Create New Post</Link>
           </Button>
         </div>
+
+        <div className="flex flex-col space-y-4">
+          {posts.map((post) => {
+            return (
+              <Link href={"/dashboard/" + post.uuid} key={post.id}>
+                <h2 className="text-lg font-medium">{post.title}</h2>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
+}
+
+async function fetchDashboardPageData() {
+  const client = getSupabaseServerComponentClient();
+  const sessionResponse = await client.auth.getSession();
+  const user = sessionResponse.data?.session?.user;
+
+  if (!user) {
+    redirect("/auth/sign-in");
+  }
+
+  const { data, error } = await fetchPosts(client, user.id);
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
 }
 
 export default DashboardPage;
